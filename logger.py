b1748cbf2da262b9ee2f5446299cf6ea6a6d0599
forchtj <j964350@vt.edu>
2014-12-15 20:42:44 -0500

    added getLastXMin()

--- logger.py
+++ logger.py
@@ -26,17 +26,30 @@ def getIntervalPoints(string1, string2):
 	result = db.query(dataquery)
 	return result
 
-#returns the last x entries with timestamps within the previous 'hours'
+#returns the average value between the given two time points
+def getIntervalAverage(string1, string2):
+	db = InfluxDBClient('localhost', '8086', 'root', 'root', 'power')
+	dataquery = 'select mean(instant_power) from sensor where time > ' + '\''+string1+'\'' + ' and time < ' + '\''+string2 +'\''
+	result = db.query(dataquery)
+	return result
+
+#returns the last x entries with timestamps within the previous 'hours' within a maximum x data points
 def getLastXPoints(hours, x):
 	db = InfluxDBClient('localhost', '8086', 'root', 'root', 'power')
 	dataquery = 'select instant_power from sensor where time > now() - ' + str(hours) + 'h limit ' + str(x)
 	result = db.query(dataquery)
 	return result
 
-#returns the average power between te given time points
+#returns the average power in the last 'hours' hours within a maximum x data points
 def getLastXAverage(hours, x):
 	db = InfluxDBClient('localhost', '8086', 'root', 'root', 'power')
 	dataquery = 'select mean(instant_power) from sensor where time > now() - ' + str(hours) + 'h limit ' + str(x)
 	result = db.query(dataquery)
-	print result[0]
-
+	return result
+	
+#returns the minimum power in last x data points within last 'hours' hours
+def getlastXMin(hours, x):
+	db = InfluxDBClient('localhost', '8086', 'root', 'root', 'power')
+	dataquery = 'select min(instant_power) from sensor where time > now() - ' + str(hours) + 'h limit ' + str(x)
+	result = db.query(dataquery)
+	return result
